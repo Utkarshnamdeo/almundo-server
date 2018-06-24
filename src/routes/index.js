224f76077f0e3074f7cc3c64762c5620e7de3ac8
Utkarsh Namdeo <utkarshnamdeo@live.com>
2018-06-25 01:02:02 +0530

    cors modified

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,16 @@
+const cors = require('cors');
 const hotelRoutes = require('./Hotel');
 
-
-module.exports =  {
+module.exports = {
     init: (app) => {
         app.use(hotelRoutes);
 
         // CORS
-        process.env.CORS_ENABLED ? app.use(cors) : null;
+        app.use(cors);
 
         // Catch all the mismatch routes
-		app.get('/*', notFound);
-		app.post('/*', notFound);
-    }
-}
-
-
-function cors (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, client-security-token, Origin, Content-Length, X-Requested-With');
-    if ('OPTIONS' == req.method) {
-        return res.send(200);
-    } else {
-        next();
+        app.get('/*', notFound);
+        app.post('/*', notFound);
     }
 }
 
